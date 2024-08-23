1225a7669717ad51bf7cc536445cc66fd0b99d81
Shaunnn_M <shaunnn-m@outlook.com>
2024-08-23 15:06:16 +0200

    LUT graph values completed

--- Prac2/Core/Src/main.c
+++ Prac2/Core/Src/main.c
@@ -19,10 +19,12 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 
+
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include <stdio.h>
 #include "stm32f0xx.h"
+#include "lcd_stm32f0.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -33,9 +35,9 @@
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
 // TODO: Add values for below variables
-#define NS        // Number of samples in LUT
-#define TIM2CLK   // STM Clock frequency
-#define F_SIGNAL  // Frequency of output analog signal
+#define NS 128    // Number of samples in LUT defined to be 128
+#define TIM2CLK 8000000  // STM Clock frequency set to 8MHz
+#define F_SIGNAL 100// Frequency of output analog signal - 1kHz
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -48,16 +50,47 @@ TIM_HandleTypeDef htim2;
 TIM_HandleTypeDef htim3;
 DMA_HandleTypeDef hdma_tim2_ch1;
 
+
 /* USER CODE BEGIN PV */
 // TODO: Add code for global variables, including LUTs
 
-uint32_t Sin_LUT[NS] = {};
-uint32_t saw_LUT[NS] = {};
-uint32_t triangle_LUT[NS] = {};
+uint32_t Sin_LUT[NS] = {511, 536, 561, 587, 611, 636, 660, 684, 708, 731, 754, 776,
+		797, 817, 837, 856, 875, 892, 908, 924, 938, 951, 964, 975, 985, 994, 1001,
+		1008, 1013, 1017, 1020, 1022, 1022, 1021, 1019, 1015, 1011, 1005, 998, 989,
+		980, 969, 958, 945, 931, 916, 900, 883, 866, 847, 827, 807, 786, 765, 742,
+		720, 696, 673, 648, 624, 599, 574, 549, 524, 498, 473, 448, 423, 398, 374,
+		349, 326, 302, 280, 257, 236, 215, 195, 175, 156, 139, 122, 106, 91, 77, 64,
+		53, 42, 33, 24, 17, 11, 7, 3, 1, 0, 0, 2, 5, 9, 14, 21, 28, 37, 47, 58, 71,
+		84, 98, 114, 130, 147, 166, 185, 205, 225, 246, 268, 291, 314, 338, 362, 386,
+		411, 435, 461, 486, 511};
+uint32_t saw_LUT[NS] = {0, 8, 16, 24, 32, 40, 48, 56, 64, 72, 81, 89, 97, 105, 113,
+		121, 129, 137, 145, 153, 161, 169, 177, 185, 193, 201, 209, 217, 226, 234,
+		242, 250, 258, 266, 274, 282, 290, 298, 306, 314, 322, 330, 338, 346, 354,
+		362, 371, 379, 387, 395, 403, 411, 419, 427, 435, 443, 451, 459, 467, 475,
+		483, 491, 499, 507, 516, 524, 532, 540, 548, 556, 564, 572, 580, 588, 596,
+		604, 612, 620, 628, 636, 644, 652, 661, 669, 677, 685, 693, 701, 709, 717,
+		725, 733, 741, 749, 757, 765, 773, 781, 789, 797, 806, 814, 822, 830, 838,
+		846, 854, 862, 870, 878, 886, 894, 902, 910, 918, 926, 934, 942, 951, 959,
+		967, 975, 983, 991, 999, 1007, 1015, 1023};
+uint32_t triangle_LUT[NS] = {0, 16, 32, 48, 64, 81, 97, 113, 129, 145, 161, 177, 193,
+		209, 226, 242, 258, 274, 290, 306, 322, 338, 354, 371, 387, 403, 419, 435,
+		451, 467, 483, 499, 516, 532, 548, 564, 580, 596, 612, 628, 644, 661, 677,
+		693, 709, 725, 741, 757, 773, 789, 806, 822, 838, 854, 870, 886, 902, 918,
+		934, 951, 967, 983, 999, 1015, 1015, 999, 983, 967, 951, 934, 918, 902, 886,
+		870, 854, 838, 822, 806, 789, 773, 757, 741, 725, 709, 693, 677, 661, 644, 628,
+		612, 596, 580, 564, 548, 532, 516, 499, 483, 467, 451, 435, 419, 403, 387, 371,
+		354, 338, 322, 306, 290, 274, 258, 242, 226, 209, 193, 177, 161, 145, 129, 113,
+		97, 81, 64, 48, 32, 16, 0};
+
+
+enum Waveform { SINE, SAWTOOTH, TRIANGLE };
+enum Waveform current_waveform = SINE;
+uint32_t *current_LUT = Sin_LUT;
 
 // TODO: Equation to calculate TIM2_Ticks
 
-uint32_t TIM2_Ticks = 0; // How often to write new LUT value
+uint32_t TIM2_Ticks = TIM2CLK / (NS * F_SIGNAL); // How often to write new LUT value
+
 uint32_t DestAddress = (uint32_t) &(TIM3->CCR3); // Write LUT TO TIM3->CCR3 to modify PWM duty cycle
 /* USER CODE END PV */
 
@@ -83,7 +116,10 @@ void EXTI0_1_IRQHandler(void);
 int main(void)
 {
 
+
   /* USER CODE BEGIN 1 */
+//  init_LCD();  // Initialize the LCD
+//  lcd_command(CLEAR);  // Clear the display to start fresh
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -105,22 +141,36 @@ int main(void)
   MX_DMA_Init();
   MX_TIM2_Init();
   MX_TIM3_Init();
+//  init_LCD();
   /* USER CODE BEGIN 2 */
 
-  // TODO: Start TIM3 in PWM mode on channel 3
+  /* USER CODE BEGIN 2 */
 
+  // Start TIM3 in PWM mode on channel 3
+  if (HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_3) != HAL_OK)
+  {
+      // PWM Generation Error
+      Error_Handler();
+  }
 
-  // TODO: Start TIM2 in Output Compare (OC) mode on channel 1.
+  // Start TIM2 in Output Compare (OC) mode on channel 1
+  if (HAL_TIM_OC_Start_IT(&htim2, TIM_CHANNEL_1) != HAL_OK)
+  {
+      // Output Compare Error
+      Error_Handler();
+  }
 
+  // Start DMA in IT mode on TIM2->CH1; Source is Sin_LUT and Dest is TIM3->CCR3
+  if (HAL_DMA_Start_IT(&hdma_tim2_ch1, (uint32_t)Sin_LUT, DestAddress, NS) != HAL_OK)
+  {
+      // DMA Transfer Error
+      Error_Handler();
+  }
 
-  // TODO: Start DMA in IT mode on TIM2->CH1; Source is LUT and Dest is TIM3->CCR3; start with Sine LUT
-
-
-  // TODO: Write current waveform to LCD ("Sine")
-  delay(3000);
+  HAL_Delay(3000);
 
   // TODO: Enable DMA (start transfer from LUT to CCR)
-
+  __HAL_TIM_ENABLE_DMA(&htim2, TIM_DMA_CC1);
 
   /* USER CODE END 2 */
 
@@ -320,16 +370,16 @@ static void MX_GPIO_Init(void)
   LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOA);
   LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOB);
 
-  /**/
+  //
   LL_SYSCFG_SetEXTISource(LL_SYSCFG_EXTI_PORTA, LL_SYSCFG_EXTI_LINE0);
 
-  /**/
+  //
   LL_GPIO_SetPinPull(Button0_GPIO_Port, Button0_Pin, LL_GPIO_PULL_UP);
 
-  /**/
+  //
   LL_GPIO_SetPinMode(Button0_GPIO_Port, Button0_Pin, LL_GPIO_MODE_INPUT);
 
-  /**/
+  //
   EXTI_InitStruct.Line_0_31 = LL_EXTI_LINE_0;
   EXTI_InitStruct.LineCommand = ENABLE;
   EXTI_InitStruct.Mode = LL_EXTI_MODE_IT;
@@ -345,15 +395,57 @@ static void MX_GPIO_Init(void)
 /* USER CODE BEGIN 4 */
 void EXTI0_1_IRQHandler(void)
 {
-	// TODO: Debounce using HAL_GetTick()
+	 	static uint32_t last_press_time = 0;
+	    uint32_t current_time = HAL_GetTick();
 
+	    // Debounce: Ensure at least 200ms have passed since the last button press
+	    if (current_time - last_press_time > 200)
+	    {
+	        // Switch waveform on button press
+	        switch (current_waveform)
+	        {
+	            case SINE:
+	                current_waveform = SAWTOOTH;
+	                current_LUT = saw_LUT;
+//	                lcd_putstring("Waveform: Sawtooth");
+	                break;
+	            case SAWTOOTH:
+	                current_waveform = TRIANGLE;
+	                current_LUT = triangle_LUT;
+//	                lcd_putstring("Waveform: Triangle");
+	                break;
+	            case TRIANGLE:
 
-	// TODO: Disable DMA transfer and abort IT, then start DMA in IT mode with new LUT and re-enable transfer
-	// HINT: Consider using C's "switch" function to handle LUT changes
+	            default:
+	                current_waveform = SINE;
+	                current_LUT = Sin_LUT;
+//	                lcd_putstring("Waveform: Sine");
+	                break;
+	        }
 
+	        // Abort the current DMA transfer
+	        if (HAL_DMA_Abort(&hdma_tim2_ch1) != HAL_OK)
+	        {
+	            // DMA Abort Error
+	            Error_Handler();
+	        }
 
+	        // Start DMA in IT mode with the new LUT
+	        if (HAL_DMA_Start_IT(&hdma_tim2_ch1, (uint32_t)current_LUT, DestAddress, NS) != HAL_OK)
+	        {
+	            // DMA Transfer Error
+	            Error_Handler();
+	        }
 
-	HAL_GPIO_EXTI_IRQHandler(Button0_Pin); // Clear interrupt flags
+	        // Re-enable the DMA request
+	        __HAL_TIM_ENABLE_DMA(&htim2, TIM_DMA_CC1);
+
+	        // Update the last press time
+	        last_press_time = current_time;
+	    }
+
+	    // Clear the EXTI line pending bit
+	    HAL_GPIO_EXTI_IRQHandler(Button0_Pin);
 }
 /* USER CODE END 4 */
 
